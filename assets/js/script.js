0f446dd93546c406de043661c4c4eb634a883df1
Marly Valcin <marlyvalcin@gmail.com>
2024-07-12 20:10:33 -0400

    updated the handleDrop function to be able to drop a card based on the task id

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,7 +79,15 @@ function handleDeleteTask(event){
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+    const taskId = ui.draggable[0].dataset.taskId;
+    const newStatus = event.target.id;
+    for(let task of taskList) {
+        if(task.id === Number(taskId)) {
+            task.status = newStatus;
+        }
+    }
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+    renderTaskList();
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
